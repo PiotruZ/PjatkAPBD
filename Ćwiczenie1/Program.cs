f05739fb06924960dc6d792f59a6071803e7c0fe
s24092 <s24092@pjwstk.edu.pl>
2024-03-24 16:22:19 +0100

    metoda na wyliczanie średniej

--- Ćwiczenie1/Program.cs
+++ Ćwiczenie1/Program.cs
@@ -3,3 +3,18 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Modyfikacja 1");
 Console.WriteLine("Modyfikacja 2");
 Console.WriteLine("Modyfikacja 3");
+
+static double CalculateAverage(int[] numbers)
+{
+    if (numbers == null || numbers.Length == 0)
+    {
+        throw new ArgumentException("Tablica nie może być pusta");
+    }
+
+    double sum = 0;
+    foreach (int number in numbers)
+    {
+        sum += number;
+    }
+    return sum / numbers.Length;
+}
